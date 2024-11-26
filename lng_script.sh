c5e9a2a7bdc8fa0a1935e73fb47e0111f336c8ce
Surya Gunasekaran <111126172+Surya-guna-23@users.noreply.github.com>
2024-11-26 18:15:00 +0530

    Create lng_script.sh

---
+++ lng_script.sh
@@ -0,0 +1,10 @@
+if test -d "./accelerator-home-ui/dist/es6/index.html"; then
+    echo "File exists."
+    sed -i "/<\/head>/i <script type=\"text/javascript\"> \
+              const fbEndpoint = new URLSearchParams(window.location.search).get('__firebolt_endpoint'); \
+              if (fbEndpoint) { \
+                  window.__firebolt = window.__firebolt || {}; \
+                  window.__firebolt.endpoint = fbEndpoint; \
+              } \
+    </script>" ./accelerator-home-ui/dist/es6/index.html
+fi
